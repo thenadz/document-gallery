2a971b7a872bdc650749afd3af65ee6420a7b647
Dan Rossiter <dan.rossiters@gmail.com>
2013-01-06 13:35:49 +0000

    Tagging v1.0
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery/tags/1.0@648689 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- document-gallery.php
+++ document-gallery.php
@@ -2,155 +2,256 @@
 /*
 Plugin Name: Document Gallery
 Description: Display non-images in gallery format on page.
-Version: 0.8.5
+Version: 1.0
 Author: Dan Rossiter
 Author URI: http://danrossiter.org/
 License: GPL2
 */
 
-/* much credit to: tgrayimages.com/automate-file-attachments-on-your-wordpress-posts/ */
-add_action( 'wp_print_styles', 'add_header_css'); 
+define( 'DG_URL', plugin_dir_url().'document-gallery/' );
 
-function add_header_css() {
-  wp_enqueue_style('document-gallery-css', plugins_url('style.css', __FILE__));
-}
-
-function get_attachment_icons($atts){
-	extract(shortcode_atts(array(
+// CREATE GALLERY STRING //
+function dg_get_attachment_icons($atts) {
+	extract( shortcode_atts( array(
 		'descriptions'		=> FALSE,
 		'echo'			=> FALSE,
 		'orderby'		=> 'menu_order',
 		'order'			=> 'ASC'
-     ), $atts));
+	), $atts) );
 	 
-	if($descriptions){ 
-		$sAttachmentString = "<table id='documentIconsWrapper'> \n"; 
-	} else{ 
-		$sAttachmentString = "<div id='documentIconsWrapper'> \n"; 
-	}
 	$args = array(
 			'numberposts'		=> -1,
-			'orderby'			=> $orderby,
-			'order'				=> $order,
-			'post_type'			=> 'attachment',
-			'post_mime_type'	=> 'application',
+			'orderby'		=> $orderby,
+			'order'			=> $order,
+			'post_type'		=> 'attachment',
+			'post_mime_type'	=> 'application,video,text,audio',
 			'post_parent'		=> get_the_ID() );
 	
-	if ( $attachments = get_posts($args) ){
-		$count = 0;
-		foreach( $attachments as $attachment ){ //setup array for more than one file attachment
-		 	$attachment_url			= wp_get_attachment_url($attachment->ID);
-		 	$attachment_name_array	= explode("/",$attachment_url);
-		 	$attachment_title		= wp_get_attachment_link($attachment->ID);
-		 	
-			if($descriptions){
-				$sAttachmentString .= "<tr><td class='documentIcons'><a href='$attachment_url'>";
-			} else{
-				$sAttachmentString .= "<div class='documentIcons'><a href='$attachment_url'>";
-			}
-			
-			switch(get_post_mime_type( $attachment->ID )){
-				case 'application/pdf': // pdf files
-					$sAttachmentString .= "<img src='".plugins_url('images/pdf.png', __FILE__)."'/>";
-					break;
-				case 'application/msword': // word files
-					$sAttachmentString .= "<img src='".plugins_url('images/word.png' , __FILE__)."'/>";
-					break;
-				case 'application/vnd.ms-powerpoint': // powerpoint files
-					$sAttachmentString .= "<img src='".plugins_url('images/PowerPoint.png' , __FILE__)."'/>";
-					break;
-				case 'application/vnd.ms-excel': // excel files
-					$sAttachmentString .= "<img src='".plugins_url( 'images/XLS8.png' , __FILE__)."'/>";
-					break;
-				case 'application/zip': // zip files
-					$sAttachmentString .= "<img src='".plugins_url( 'images/zip.png' , __FILE__)."'/>";
-					break;
-				case 'application/vnd.oasis.opendocument.text': // OpenDocument (text)
-					$sAttachmentString .= "<img src='".plugins_url( 'images/open-text.png' , __FILE__)."'/>";
-					break;
-				case 'application/vnd.oasis.opendocument.spreadsheet': // OpenDocument (spreadsheet)
-					$sAttachmentString .= "<img src='".plugins_url( 'images/open-spreadsheet.png' , __FILE__)."'/>";
-					break;
-				case 'application/vnd.oasis.opendocument.presentation': // OpenDocument (presentation)
-					$sAttachmentString .= "<img src='".plugins_url( 'images/open-presentation.png' , __FILE__)."'/>";
-					break;
-				case 'application/vnd.oasis.opendocument.database': // OpenDocument (database)
-					$sAttachmentString .= "<img src='".plugins_url( 'images/open-database.png' , __FILE__)."'/>";
-					break;
-				case 'application/vnd.oasis.opendocument.formula': // OpenDocument (formula)
-					$sAttachmentString .= "<img src='".plugins_url( 'images/open-formula.png' , __FILE__)."'/>";
-					break;
-				default:
-					continue;
-			}
-			
-			$sAttachmentString .= "</a><br><a href='$attachment_url'>$attachment_title</a>";
-			if($descriptions){
-				$sAttachmentString .= "</td><td valign='top'><p>$attachment->post_content</p></td></tr>";
-			}else{
-				$sAttachmentString .= "</div>";
-				if(++$count % 4 == 0)
-					$sAttachmentString .= "<hr>";
-			}
+	if ( $attachments = get_posts($args) ) {
+		$attachment_str = array( '<!-- GENERATED USING DOCUMENT GALLERY'.PHP_EOL.
+					 '     http://wordpress.org/extend/plugins/document-gallery -->'.PHP_EOL );
+
+		if($descriptions) {
+			$attachment_str[] = '<table id="document-icon-wrapper">'; 
+		} else { 
+			$attachment_str[] = '<div id="document-icon-wrapper">'; 
 		}
-	}
 
-	//Audio Files
-	/*$mp3s = get_children(array(   //do only if there are attachments of these qualifications
-	 'post_parent' => get_the_ID(),
-	 'post_type' => 'attachment',
-	 'numberposts' => -1,
-	 'post_mime_type' => 'audio',  //MIME Type condition
-	 ) );
+		$count = 0;
+		foreach( $attachments as $attachment ) { //setup array for more than one file attachment
+		 	$url	= wp_get_attachment_url( $attachment->ID );
+		 	$title	= wp_get_attachment_link( $attachment->ID );
+			$icon	= dg_get_attachment_icon( $attachment->ID, $tile, $url );
+			
+			if($descriptions) {
+				$attachment_str[] = '<tr><td class="document-icon">';
+			} else {
+				$attachment_str[] = '<div class="document-icon">';
+			}
 
-	if (!empty($mp3s)) :
-	$sAttachmentString .= "<ul class='audiofiles'>";
-		foreach($mp3s as $mp3) :
-    		$sAttachmentString .= "<li>";
-			if(!empty($mp3->post_title)) : //checking to make sure the post title isn't empty
-				$sAttachmentString .= "<h4 class='title'>".$mp3->post_title."</h4>";
-			endif;
+			$attachment_str[] = "<a href=\"$url\">$icon</a><br><a href=\"$url\">$title</a>";
 
-			if(!empty($mp3->post_content)) : //checking to make sure something exists in post_content (description)
-				$sAttachmentString .= "<p class='description'>".$mp3->post_content."</p>";
-			endif;
+			if($descriptions) {
+				$attachment_str[] = "</td><td valign=\"top\"><p>$attachment->post_content</p></td></tr>";
+			} else {
+				$attachment_str[] = '</div>';
+				if( ++$count % 4 == 0 ) {
+					$attachment_str[] = '<hr>';
+				}
+			}
+		} // end looping attachments
 
-			$sAttachmentString .= "<object width='470' height='24' id='single".$mp3->ID."' name='single".$mp3->ID."'>";
-				$sAttachmentString .= "<param name='movie' value='player.swf'>";
-				$sAttachmentString .= "<param name='allowfullscreen' value='true'>";
-				$sAttachmentString .= "<param name='allowscriptaccess' value='always'>";
-				$sAttachmentString .= "<param name='wmode' value='transparent'>";
-				$sAttachmentString .= "<param name='flashvars' value='file=".$mp3->guid."'>";
-					$sAttachmentString .= "<embed ";
-					  $sAttachmentString .= "id='single".$mp3->ID."' ";
-					  $sAttachmentString .= "name='single".$mp3->ID."' ";
-					  $sAttachmentString .= "src='".get_bloginfo('template_directory')."/jw/player.swf' ";
-					  $sAttachmentString .= "width='470' ";
-					  $sAttachmentString .= "height='24' ";
-					  $sAttachmentString .= "bgcolor='#ffffff' ";
-					  $sAttachmentString .= "allowscriptaccess='always' ";
-					  $sAttachmentString .= "allowfullscreen='true' ";
-					  $sAttachmentString .= "flashvars='file=".$mp3->guid."' ";
+		// close #document-icon-wrapper
+		if($descriptions) {
+			$attachment_str[] = '</table>';
+		} else {
+			$attachment_str[] = '</div>';
+		}
 
-					$sAttachmentString .= "/>";
-			$sAttachmentString .= "</object>";
-                        $sAttachmentString .= "<a href='".$mp3->guid."'>Download</a>";
-			$sAttachmentString .= "</li>";
-		endforeach;
-	$sAttachmentString .= "</ul>";
-	endif;*/
-
-	if($descriptions){
-		$sAttachmentString .= "</table>";
-	}else{
-		$sAttachmentString .= "</div>";
-	}
-	/*$finalString = implode('', $sAttachmentString);*/
-	if($echo){
-	    /*echo $finalString;*/
-		echo $sAttachmentString;
-	}
-	return $sAttachmentString; /*$finalString;*/
+		// join array & return
+		$attachment_str = implode( '', $attachment_str );
+		return $attachment_str;
+	} // end if attachments
+	
+	return '<!-- Document Gallery: No attachments to display. -->'.PHP_EOL;
 }
-add_shortcode('document gallery', 'get_attachment_icons');
+add_shortcode('document gallery', 'dg_get_attachment_icons');
+add_shortcode('dg', 'dg_get_attachment_icons');
+
+
+// ADD SOME STYLING //
+function dg_add_header_css() {
+	wp_enqueue_style( 'document-gallery-css', plugins_url('style.css', __FILE__) );
+}
+add_action( 'wp_print_styles', 'dg_add_header_css'); 
+
+
+// HELPERS //
+
+// pass in $title & $url to avoid mult function calls
+function dg_get_attachment_icon( $id, $title, $url ) {
+	$filename = basename( $url );
+	$filetype = wp_check_filetype( $filename );
+
+	// identify extension
+	switch( $filetype['ext'] ) {
+		// Most Common First
+		case 'pdf':
+			$icon = 'pdf.png';
+			break;
+		// MS Office
+		case 'doc':
+		case 'docx':
+		case 'docm':
+		case 'dotx':
+		case 'dotm':
+			$icon = 'msdoc.png';
+			break;
+		case 'ppt':
+		case 'pot':
+		case 'pps':
+		case 'pptx':
+		case 'pptm':
+		case 'ppsx':
+		case 'ppsm':
+		case 'potx':
+		case 'potm':
+		case 'ppam':
+		case 'sldx':
+		case 'sldm':
+			$icon = 'msppt.png';
+			break;
+		case 'xla':
+		case 'xls':
+		case 'xlt':
+		case 'xlw':
+		case 'xlsx':
+		case 'xlsm':
+		case 'xlsb':
+		case 'xltx':
+		case 'xltm':
+		case 'xlam':
+			$icon = 'msxls.png';
+			break;
+		case 'mdb':
+			$icon = 'msaccess.png';
+			break;
+		// Video formats
+		case 'avi':
+			$icon = 'avi.png';
+			break;
+		case 'divx':
+			$icon = 'divx.png';
+			break;
+		case 'flv':
+			$icon = 'flv.png';
+			break;
+		case 'qt':
+		case 'mov':
+			$icon = 'mov.png';
+			break;
+		case 'asf':
+		case 'asx':
+		case 'wax':
+		case 'wmv':
+		case 'wmx':
+			$icon = 'wmv.png';
+			break;
+		case 'mkv':
+			$icon = 'mkv.png';
+			break;
+		// Audio formats
+		case 'mp3':
+			$icon = 'mp3.png';
+			break;
+		case 'wav':
+			$icon = 'wav.png';
+			break;
+		case 'ogg':
+		case 'oga':
+			$icon = 'ogg.png';
+			break;
+		case 'midi':
+		case 'mid':
+			$icon = 'midi.png';
+			break;
+		case 'wma':
+			$icon = 'wma.png';
+			break;
+		// Text formats
+		case 'rtx':
+			$icon = 'rtx.png';
+			break;
+		case 'ics':
+			$icon = 'ics.png';
+			break;
+		case 'csv':
+			$icon = 'csv.png';
+			break;
+		// Msc application formats
+		case 'html':
+		case 'htm': // death to all who use this!
+			$icon = 'html.png';
+			break;
+		case 'css':
+			$icon = 'css.png';
+			break;
+		case 'js':
+			$icon = 'javascript.png';
+			break;
+		case 'class':
+			$icon = 'java.png';
+			break; 
+		case 'zip':
+			$icon = 'zip.png';
+			break;
+		case 'tar':
+		case 'gzip':
+		case 'gz':
+		case 'bz2': // not yet WP-supported
+		case 'tgz': // not yet WP-supported
+			$icon = 'compressed.png';
+			break;
+		case 'rar': // RAWR!!!
+			$icon = 'rar.png';
+			break;
+		case '7z':
+			$icon = '7zip.png';
+			break;
+		case 'exec':
+			$icon = 'exec.png';
+			break;
+		case 'rtf':
+			$icon = 'rtf.png';
+			break;
+		case 'swf':
+			$icon = 'shockwave.png';
+			break;
+		// OpenOffice formats
+		case 'odt':
+			$icon = 'opendocument-text.png';
+			break;
+		case 'odp':
+			$icon = 'opendocument-presentation.png';
+			break;
+		case 'ods':
+			$icon = 'opendocument-spreadsheet.png';
+			break;
+		case 'odg':
+			$icon = 'opendocument-graphics.png';
+			break;
+		case 'odb':
+			$icon = 'opendocument-database.png';
+			break;
+		case 'odf':
+			$icon = 'opendocument-formula.png';
+			break;
+		// fallback to default icons if not recognized
+		default:
+			return get_attachment_icon( $id );
+	}
+
+	$icon = '<img src="'.DG_URL.'icons/'.$icon."\" title=\"$title\" alt=\"$title\"/>";
+	return $icon;
+}
 ?>
