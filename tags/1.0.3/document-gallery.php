e39a7004198b4f1e3e0fefe1772c4bca832b6b8f
thenadz <dan.rossiters@gmail.com>
2013-01-07 18:46:28 +0000

    Tagging 1.0.3: Resolved bug with determing plugin directory. Doh!
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery@649300 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.0.3/document-gallery.php
+++ tags/1.0.3/document-gallery.php
@@ -8,16 +8,15 @@ Author URI: http://danrossiter.org/
 License: GPL2
 */
 
-define( 'DG_URL', plugin_dir_url('document-gallery/') );
+define( 'DG_URL', plugin_dir_url( __FILE__ ) );
 
 // CREATE GALLERY STRING //
 function dg_get_attachment_icons($atts) {
 	extract( shortcode_atts( array(
 		'descriptions'		=> FALSE,
+		'echo'			=> FALSE,
 		'orderby'		=> 'menu_order',
-		'order'			=> 'ASC',
-		'attachment_pg'		=> FALSE, // link directly to file (true to link to attachment pg)
-		'ids'			=> FALSE // not yet supported
+		'order'			=> 'ASC'
 	), $atts) );
 	 
 	$args = array(
@@ -29,23 +28,18 @@ function dg_get_attachment_icons($atts) {
 			'post_parent'		=> get_the_ID() );
 	
 	if ( $attachments = get_posts($args) ) {
-		$attachment_str = array( PHP_EOL.'<!-- GENERATED USING DOCUMENT GALLERY'.PHP_EOL.
-					 'http://wordpress.org/extend/plugins/document-gallery -->'.PHP_EOL );
+		$attachment_str = array( '<!-- GENERATED USING DOCUMENT GALLERY'.PHP_EOL.
+					 '     http://wordpress.org/extend/plugins/document-gallery -->'.PHP_EOL );
 
-		if( $descriptions ) {
+		if($descriptions) {
 			$attachment_str[] = '<table id="document-icon-wrapper">'; 
 		}
 
 		$count = 0;
-		foreach( $attachments as $attachment ) { //setup array for more than one file attachment	
-			if( $attachment_pg ) {
-				$url = get_attachment_link( $attachment->ID );
-			} else {
-				$url = wp_get_attachment_url( $attachment->ID );
-			}
-
+		foreach( $attachments as $attachment ) { //setup array for more than one file attachment
+		 	$url	= wp_get_attachment_url( $attachment->ID );
 		 	$title	= get_the_title( $attachment->ID );
-			$icon	= get_attachment_icon( $attachment->ID );
+			$icon	= dg_get_attachment_icon( $attachment->ID, $tile, $url );
 			
 			if($descriptions) {
 				$attachment_str[] = '<tr><td class="document-icon">';
@@ -96,9 +90,9 @@ add_action( 'wp_print_styles', 'dg_add_header_css');
 // HELPERS //
 
 // pass in $title & $url to avoid mult function calls
-function dg_get_attachment_icon( $icon, $id) {
-	$url = wp_get_attachment_url( $id );
-	$filetype = wp_check_filetype( basename( $url ) );
+function dg_get_attachment_icon( $id, $title, $url ) {
+	$filename = basename( $url );
+	$filetype = wp_check_filetype( $filename );
 
 	// identify extension
 	switch( $filetype['ext'] ) {
@@ -253,12 +247,12 @@ function dg_get_attachment_icon( $icon, $id) {
 		case 'odf':
 			$icon = 'opendocument-formula.png';
 			break;
-		default: // fallback to default icon if no match
-			return $icon;
+		// fallback to default icons if not recognized
+		default:
+			return get_attachment_icon( $id );
 	}
 
 	$icon = '<img src="'.DG_URL.'icons/'.$icon."\" title=\"$title\" alt=\"$title\"/>";
 	return $icon;
 }
-add_filter( 'attachment_icon', 'dg_get_attachment_icon', 10, 2 );
 ?>
