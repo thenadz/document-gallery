5e760114bd2d100f08570cdc970769fa5d9ba88f
thenadz <dan.rossiters@gmail.com>
2013-01-12 12:38:39 +0000

    Setting stable version and other version numbering stuff
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery@651636 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.0.4/document-gallery.php
+++ tags/1.0.4/document-gallery.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Document Gallery
 Description: Display non-images in gallery format on page.
-Version: 1.0.3
+Version: 1.0.4
 Author: Dan Rossiter
 Author URI: http://danrossiter.org/
 License: GPL2
