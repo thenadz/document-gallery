069ce71c1a4ccd64071605faa697ceaf5df62df9
Dan Rossiter <dan.rossiters@gmail.com>
2014-03-15 06:20:43 +0000

    Beta (trunk) updates: * Made much of DG functionality configurable through the new Settings page. * Changed handling of thumbnail metadata to significantly speed up processing. * Added i18n and L10n framework. * Lots of general improvements across the plugin, including removing all raw functions (all function definitions now resides in a class).
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery@875789 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/document-gallery.php
+++ trunk/document-gallery.php
@@ -1,4 +1,5 @@
 <?php
+defined('WPINC') OR exit;
 
 /*
   Plugin Name: Document Gallery
@@ -7,10 +8,12 @@
   Author: Dan Rossiter
   Author URI: http://danrossiter.org/
   License: GPLv2
+  Text Domain: document-gallery
  */
 
+// define helper paths & URLs
 define('DG_URL', plugin_dir_url(__FILE__));
-define('DG_PATH', trailingslashit(dirname(__FILE__)));
+define('DG_PATH', plugin_dir_path(__FILE__));
 if(!defined('WP_INCLUDE_DIR')) {
    define('WP_INCLUDE_DIR', preg_replace('/wp-content$/', 'wp-includes', WP_CONTENT_DIR));
 }
@@ -18,42 +21,136 @@ if(!defined('WP_ADMIN_DIR')) {
    define('WP_ADMIN_DIR', preg_replace('/wp-content$/', 'wp-admin', WP_CONTENT_DIR));
 }
 
+// init DG options for use throughout plugin
+global $dg_options;
+define('DG_OPTION_NAME', 'document_gallery');
+$dg_options = get_option(DG_OPTION_NAME);
+
+// handle activation and uninstallation
+include_once DG_PATH . 'inc/class-setup.php';
+register_activation_hook(__FILE__, array('DG_Setup', 'activation'));
+register_uninstall_hook(__FILE__, array('DG_Setup', 'uninstall'));
+
+// I18n
+add_action('plugins_loaded', array('DocumentGallery', 'loadTextDomain'));
+
+// cleanup cached data when thumbed attachment deleted
+include_once DG_PATH . 'inc/class-thumber.php';
+add_action('delete_attachment', array('DG_Thumber', 'deleteThumbMeta'));
+
+if (is_admin()) {
+   // admin house keeping
+   include_once DG_PATH . 'admin/class-admin.php';
+
+   // add settings link
+   add_filter('plugin_action_links_' . plugin_basename(__FILE__),
+       array('DG_Admin', 'addSettingsLink'));
+
+   // build options page
+   add_action('admin_menu', array('DG_Admin', 'addAdminPage'));
+   if (!empty($GLOBALS['pagenow'])
+       && ('options-general.php' === $GLOBALS['pagenow'] // output
+       || 'options.php' === $GLOBALS['pagenow'])) {      // validation
+       add_action('admin_init', array('DG_Admin', 'registerAdminStyle'));
+       add_action('admin_init', array('DG_Admin', 'registerSettings'));
+   }
+} else {
+   // styling for gallery
+   if (false === $dg_options['css']) {
+      // enqueue standard CSS
+      add_action('wp_enqueue_scripts', array('DocumentGallery', 'enqueueGalleryStyle'));
+   } else {
+      // print custom CSS
+      add_action('wp_print_styles', array('DocumentGallery', 'printGalleryStyle'));
+   }
+}
+
+// adds 'dg' shortcode
+add_shortcode('dg', array('DocumentGallery', 'doShortcode'));
+
 /**
- * Takes values passed from attributes and returns sutable HTML to represent
- * all valid attachments requested.
+ * DocumentGallery wraps basic functionality to setup the plugin.
  *
- * @param array $atts         Arguments from the user.
- * @return string             HTML for the Document Gallery.
+ * @author drossiter
  */
-function dg_get_attachment_icons($atts) {
-   include_once DG_PATH . 'models/class-gallery.php';
+class DocumentGallery {
 
-   // empty string is passed when no arguments are given, but constructor expects an array
-   return (string)(new DG_Gallery(empty($atts) ? array() : $atts));
+   /*==========================================================================
+    * THE SHORTCODE
+    *=========================================================================*/
+
+   /**
+    * Takes values passed from attributes and returns sutable HTML to represent
+    * all valid attachments requested.
+    *
+    * @param array $atts         Arguments from the user.
+    * @return string             HTML for the Document Gallery.
+    */
+   public static function doShortcode($atts) {
+      include_once 'inc/class-gallery.php';
+      return new DG_Gallery($atts);
+   }
+
+   /**
+    * Enqueue standard DG CSS.
+    */
+   public static function enqueueGalleryStyle() {
+      wp_register_style('dg-main', DG_URL . 'assets/css/style.css', null, self::version());
+      wp_enqueue_style('dg-main');
+   }
+
+   /**
+    * Print custom user CSS.
+    */
+   public static function printGalleryStyle() {
+      global $dg_options;
+
+      if ($dg_options['css'])
+      {
+         echo '<style type="text/css">' . PHP_EOL;
+         echo $dg_options['css'];
+         echo '</style>' . PHP_EOL;
+      }
+   }
+
+   /*==========================================================================
+    * I18n
+    *=========================================================================*/
+
+   public static function loadTextDomain() {
+      load_plugin_textdomain('document-gallery', false, DG_PATH . 'languages');
+   }
+
+   /*==========================================================================
+    * HELPER FUNCTIONS
+    *=========================================================================*/
+
+   /**
+    * Gets the current version of the plugin.
+    * @param bool recalculate Whether to re-read the file for version number.
+    * @return str DG version installed.
+    */
+   public static function version($recalculate = false) {
+      static $version = null;
+
+      if ($recalculate) {
+         include_once WP_ADMIN_DIR . '/includes/plugin.php';
+         $data = get_plugin_data(__FILE__, false);
+         $version = $data['Version'];
+      } elseif(is_null($version)) {
+         global $dg_options;
+         $version = $dg_options['version'];
+      }
+
+      return $version;
+   }
+
+   /**
+    * Blocks instantiation. All functions are static.
+    */
+   private function __construct() {
+
+   }
 }
-
-/**
- * 'document gallery' shortcode depreciated as of v1.0. left for backward compatibility
- */
-add_shortcode('document gallery', 'dg_get_attachment_icons');
-add_shortcode('dg', 'dg_get_attachment_icons');
-
-/**
- * Include stylesheet for some basic design.
- */
-function dg_add_header_css() {
-   wp_enqueue_style('document-gallery-css', DG_URL . 'style.css');
-}
-add_action('wp_print_styles', 'dg_add_header_css');
-
-/**
- * Delete thumbnail when attachment is deleted
- * @param int $ID ID of the attachment being deleted.
- */
-function dg_delete_thumb($ID) {
-   include_once DG_PATH . 'util/class-thumber.php';
-   DG_Thumber::deleteThumbMeta($ID);
-}
-add_action('delete_attachment', 'dg_delete_thumb');
 
 ?>
---
+++ trunk/inc/class-setup.php
@@ -0,0 +1,87 @@
+<?php
+defined('WPINC') OR exit;
+
+/**
+ * Holds functions that handle DG setup / uninstallation.
+ *
+ * @author drossiter
+ */
+class DG_Setup {
+
+   /**
+    * @return array Contains default options for DG.
+    */
+   public static function getDefaultOptions() {
+      return array(
+          'thumber' => array(
+              'thumbs' => array(),
+              'gs'     => DG_Thumber::getGhostscriptExecutable(),
+              'active' => DG_Thumber::getDefaultThumbers(),
+              'width'  => 200,
+              'height' => 200
+          ),
+          'gallery' => array(
+              'defaults' => array(
+                  // default: link directly to file (true to link to attachment pg)
+                  'attachment_pg'  => false,
+                  'descriptions'   => false,
+                  // include thumbnail of actual document in gallery display
+                  'fancy'          => false,
+                  // comma-separated list of attachment ids
+                  'ids'            => false,
+                  // if true, all images attached to current page will be included also
+                  'images'         => false,
+                  'localpost'      => true,
+                  'order'          => 'ASC',
+                  'orderby'        => 'menu_order',
+                  // only relevant if tax_query used (WP >= 3.1)
+                  'relation'       => 'AND'
+              )
+          ),
+          'css' => false,
+          'version' => DocumentGallery::version(true)
+      );
+   }
+
+   /**
+    * Runs when DG is activated.
+    */
+   public static function activation() {
+      $options = get_option(DG_OPTION_NAME, null);
+      if (is_null($options)) {
+         // first installation
+         add_option(DG_OPTION_NAME, self::getDefaultOptions());
+      } else if (DocumentGallery::version(true) !== $options['version']) {
+         // update version number
+         $options['version'] = DocumentGallery::version(true);
+         update_option(DG_OPTION_NAME, $options);
+      }
+   }
+
+   /**
+    * Runs when DG is uninstalled.
+    */
+   public static function uninstall() {
+      if (!current_user_can('activate_plugins')) return;
+      check_admin_referer('bulk-plugins');
+
+      $options = DG_Thumber::getOptions();
+
+      foreach ($options['thumbs'] as $val) {
+         if (false !== $val) {
+            @unlink($val['thumb_path']);
+         }
+      }
+
+      delete_option(DG_OPTION_NAME);
+   }
+
+   /**
+    * Blocks instantiation. All functions are static.
+    */
+   private function __construct() {
+
+   }
+}
+
+?>
