2fb5aba1e27f947afe32e8cecca230783000043c
thenadz <dan.rossiters@gmail.com>
2015-01-11 22:19:30 +0000

    Adding DG utility class containing rough draft of JSON encode replacement. Still needs testing.
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery@1065436 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/inc/class-util.php
@@ -0,0 +1,65 @@
+<?php
+defined('WPINC') OR exit;
+
+/**
+ * General utility function for Document Gallery.
+ *
+ * @author drossiter
+ */
+class DG_Document {
+   private static $jsonEncode;
+   
+   public static function jsonEncode($decoded) {
+      if (!isset(self::$jsonEncode)) {
+         self::$jsonEncode = 'json_encode';
+         if (!function_exists($getNativeJsonEncode)) {
+            self::$jsonEncode = array(__CLASS__, '_jsonEncode');
+         }
+      }
+      
+      // do encoding
+      $ret = call_user_func(self::$jsonEncode, $decoded);
+      if (false === $ret) {
+         DG_Logger::writeLog(DG_LogLevel::Error, 'Failed to encode JSON: ' . var_dump($decoded));
+      }
+      
+      return $ret;
+   }
+   
+   private static function _jsonEncode($decoded) {
+      $isJsonObj = is_object($decoded) || isAssociativeArray($decoded);
+      
+      if ($isJsonObj) {
+         $ret = '';
+         foreach (array_map(array(__CLASS__, __FUNCTION__), $decoded) as $k => $v) {
+            $ret .= "\"$k\":$v";
+         }
+         return "\{$ret\}";
+      } elseif (is_array($decoded)) {
+         return '[' . implode(',', array_map(array(__CLASS__, __FUNCTION__), $decoded)) . ']';
+      } elseif (is_bool($decoded)) {
+         static $boolMap = array('false', 'true');
+         return $boolMap[(int)$decoded];
+      } elseif (is_string($decoded)) {
+         return '"' . str_replace(array('\\', '"'), array('\\\\', '\\"'), $decoded) . '"';
+      } else {
+         return (string)$decoded;
+      }
+   }
+   
+   private static function isAssociativeArray($array) {
+      $ret = false;
+      
+      if (is_array($array)) {
+         $next = 0;
+         foreach (array_keys($array) as $k) {
+            if ($next++ !== $k) {
+               $ret = true;
+               break;
+            }
+         }
+      }
+      
+      return $ret;
+   }
+}
