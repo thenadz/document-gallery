bb5873c1d91c05dbc36e42148604f6454a8f55a2
thenadz <dan.rossiters@gmail.com>
2015-06-09 05:11:00 +0000

    Formatting per WordPress style guide. TODO: Functions have not yet be renamed.
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery@1177193 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/inc/class-document.php
+++ trunk/inc/class-document.php
@@ -1,5 +1,5 @@
 <?php
-defined('WPINC') OR exit;
+defined( 'WPINC' ) OR exit;
 
 /**
  * Holds data specific to a given document.
@@ -8,78 +8,77 @@ defined('WPINC') OR exit;
  */
 class DG_Document {
 
-   /*==========================================================================
-    * PRIVATE FIELDS
-    *=========================================================================*/
+	/*==========================================================================
+		* PRIVATE FIELDS
+		*=========================================================================*/
 
-   // general document data
-   private $description, $gallery, $ID, $link, $title, $title_attribute;
+	// general document data
+	private $description, $gallery, $ID, $link, $title, $title_attribute;
 
-   /*==========================================================================
-    * INIT GALLERY
-    *=========================================================================*/
+	/*==========================================================================
+		* INIT GALLERY
+		*=========================================================================*/
 
-   /**
-    * Constructs instance of Document.
-    * @param type $attachment Attachment object used to initalize fields.
-    * @param type $gallery    Instance of Gallery class.
-    */
-   public function __construct($attachment, $gallery) {
-      include_once DG_PATH . 'inc/class-thumber.php';
-      
-      // init general document data
-      $this->gallery = $gallery;
-      $this->description = wptexturize($attachment->post_content);
-      $this->ID = $attachment->ID;
-      $this->link = $gallery->linkToAttachmentPg()
-         ? get_attachment_link($attachment->ID)
-         : wp_get_attachment_url($attachment->ID);
-      $this->title = wptexturize($attachment->post_title);
-      $this->title_attribute = esc_attr(strip_tags($this->title));
-   }
+	/**
+	 * Constructs instance of Document.
+	 *
+	 * @param WP_Post $attachment Attachment object used to initalize fields.
+	 * @param DG_Gallery $gallery Instance of Gallery class.
+	 */
+	public function __construct( $attachment, $gallery ) {
+		include_once DG_PATH . 'inc/class-thumber.php';
 
-   /*==========================================================================
-    * OUTPUT HTML STRING
-    *=========================================================================*/
+		// init general document data
+		$this->gallery         = $gallery;
+		$this->description     = wptexturize( $attachment->post_content );
+		$this->ID              = $attachment->ID;
+		$this->link            = $gallery->linkToAttachmentPg()
+			? get_attachment_link( $attachment->ID )
+			: wp_get_attachment_url( $attachment->ID );
+		$this->title           = wptexturize( $attachment->post_title );
+		$this->title_attribute = esc_attr( strip_tags( $this->title ) );
+	}
 
-   /**
-    * Returns HTML representing this Document.
-    * @filter dg_icon_template Filters the DG icon HTML. Passes a single
-    *    bool value indicating whether the gallery is using descriptions or not.
-    * @return string
-    */
-   public function __toString() {
-      $thumb = $this->gallery->useFancyThumbs()
-          ? DG_Thumber::getThumbnail($this->ID)
-          : DG_Thumber::getDefaultThumbnail($this->ID);
+	/*==========================================================================
+		* OUTPUT HTML STRING
+		*=========================================================================*/
 
-      $repl = array($this->link, $thumb, $this->title_attribute, $this->title);
-      $find = array('%link%', '%img%', '%title_attribute%', '%title%');
-      $description = '';
-      
-      // if descriptions then add filterable tag and value to replaced tag
-      if ($this->gallery->useDescriptions()) {
-         $repl[] = $this->description;
-         $find[] = '%description%';
-         $description = '   <p>%description%</p>';
-      }
-      
-      $target = $this->gallery->openLinkInNewWindow() ? '_blank' : '_self';
-      $doc_icon =
-         '   <div class="document-icon">' . PHP_EOL .
-         "      <a href=\"%link%\" target=\"$target\"><img src=\"%img%\" title=\"%title_attribute%\" alt=\"%title_attribute%\" /><br>%title%</a>" . PHP_EOL .
-         '   </div>' . PHP_EOL .
-         $description;
-      
-      // allow developers to filter icon output
-      $doc_icon = apply_filters(
-         'dg_icon_template',
-         $doc_icon,
-         $this->gallery->useDescriptions(),
-         $this->ID);
+	/**
+	 * Returns HTML representing this Document.
+	 * @filter dg_icon_template Filters the DG icon HTML. Passes a single
+	 *    bool value indicating whether the gallery is using descriptions or not.
+	 * @return string
+	 */
+	public function __toString() {
+		$thumb = $this->gallery->useFancyThumbs()
+			? DG_Thumber::getThumbnail( $this->ID )
+			: DG_Thumber::getDefaultThumbnail( $this->ID );
 
-      return str_replace($find, $repl, $doc_icon);
-   }
+		$repl        = array( $this->link, $thumb, $this->title_attribute, $this->title );
+		$find        = array( '%link%', '%img%', '%title_attribute%', '%title%' );
+		$description = '';
+
+		// if descriptions then add filterable tag and value to replaced tag
+		if ( $this->gallery->useDescriptions() ) {
+			$repl[]      = $this->description;
+			$find[]      = '%description%';
+			$description = '   <p>%description%</p>';
+		}
+
+		$target   = $this->gallery->openLinkInNewWindow() ? '_blank' : '_self';
+		$doc_icon =
+			'   <div class="document-icon">' . PHP_EOL .
+			"      <a href=\"%link%\" target=\"$target\"><img src=\"%img%\" title=\"%title_attribute%\" alt=\"%title_attribute%\" /><br>%title%</a>" . PHP_EOL .
+			'   </div>' . PHP_EOL .
+			$description;
+
+		// allow developers to filter icon output
+		$doc_icon = apply_filters(
+			'dg_icon_template',
+			$doc_icon,
+			$this->gallery->useDescriptions(),
+			$this->ID );
+
+		return str_replace( $find, $repl, $doc_icon );
+	}
 }
-
-?>
