03180f9b72700f9597b15c3d4d5ac6591171b3b1
thenadz <dan.rossiters@gmail.com>
2015-10-13 04:13:47 +0000

    First attempt at adding pagination to DG. TODO: The data-shortcode attribute is being populated with incorrect JSON types, which in turn breaks gallery.js logic. Fixing the JSON generation in class-gallery *should* result in everything working, or close to it.
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery@1264602 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/assets/js/gallery.js
+++ trunk/assets/js/gallery.js
@@ -1,24 +1,64 @@
-(function() {
+(function($) {
     // distinct list of all attachment IDs populated
-    var ids = [];
+    var ids;
 
     // current index in pendingIcons
-    var i = 0;
+    var i;
 
     // find all document-icons without icons generated and start processing
-    jQuery(document).ready(function() {
-        jQuery('.document-icon[data-dg-id]').each(function() {
-            var id = jQuery(this).data('dg-id');
+    $(document).ready(function() {
+        resetPendingIcons();
+
+        $('.dg-paginate-wrapper .paginate').click(function (e) {
+            var target = $(this).closest('.dg-paginate-wrapper');
+            var atts = target.children('div[data-shortcode]').data('shortcode');
+
+            if (!atts.hasOwnProperty('skip')) {
+                atts['skip'] = 0;
+            }
+
+            if ($(this).hasClass('left')) {
+                atts['skip'] -= atts['limit'];
+            } else {
+                atts['skip'] += atts['limit'];
+            }
+
+            retrieveGallery(atts, target)
+            e.preventDefault();
+        });
+    });
+
+    /**
+     * Collects all of the DG icons that need to be generated and starts requesting them via AJAX.
+     */
+    function resetPendingIcons() {
+        ids = [];
+        i = 0;
+
+        $('.document-icon[data-id]').each(function() {
+            var id = $(this).data('id');
 
             // if we have multiple galleries, we could have multiple elements
             // needing the same icon and no need to request multiple times
-            if (-1 === jQuery.inArray(id, ids)) {
+            if (-1 === $.inArray(id, ids)) {
                 ids.push(id);
             }
         });
 
         retrieveNextIcons();
-    });
+    }
+
+    /**
+     * Requests a gallery generated with the given attributes to populate the given target element.
+     * @param atts array The gallery shortcode attributes.
+     * @param target element The element to be updated with the AJAX HTML response.
+     */
+    function retrieveGallery(atts, target) {
+        $.post(ajaxurl, { action: 'dg_generate_gallery', atts: atts }, function(html) {
+            target.replaceWith(html);
+            resetPendingIcons();
+        });
+    }
 
     /**
      * Sends AJAX request to server requesting some of the not-yet-generated icons (if any).
@@ -40,7 +80,7 @@
 
         if (idBatch.length != 0) {
             // request the next batch of icons
-            jQuery.post(ajaxurl, { action: 'dg_generate_icons', ids: idBatch }, processRetrievedThumbnails);
+            $.post(ajaxurl, { action: 'dg_generate_icons', ids: idBatch }, processRetrievedThumbnails);
         }
     }
 
@@ -51,12 +91,15 @@
     function processRetrievedThumbnails(response) {
         for (var id in response) {
             if (response.hasOwnProperty(id)) {
-                var target = jQuery('.document-icon[data-dg-id="' + id + '"] img');
+                var target = $('.document-icon[data-id="' + id + '"] img');
+                target.removeAttr('data-id');
+
                 (function(id, target) {
-                    target.fadeOut('fast', function () {
-                        jQuery(this).attr('src', response[id]);
-                        jQuery(this).fadeIn('fast');
-                    })
+                    var speed = 'fast';
+                    target.fadeOut(speed, function () {
+                        $(this).attr('src', response[id]);
+                        $(this).fadeIn(speed);
+                    });
                 })(id, target);
             }
         }
@@ -64,4 +107,4 @@
         // start next batch once this response is processed
         retrieveNextIcons();
     }
-})();
+})(jQuery);
