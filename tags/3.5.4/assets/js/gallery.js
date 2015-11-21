ee804102d422cf4a6db7bf8a2cf99ec861d2667e
thenadz <dan.rossiters@gmail.com>
2015-11-21 10:31:44 +0000

    Tagging 3.5.4 which addresses duplicate gallery ID attribtues.
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery@1291446 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.5.4/assets/js/gallery.js
@@ -0,0 +1,67 @@
+(function() {
+    // distinct list of all attachment IDs populated
+    var ids = [];
+
+    // current index in pendingIcons
+    var i = 0;
+
+    // find all document-icons without icons generated and start processing
+    jQuery(document).ready(function() {
+        jQuery('.document-icon[data-dg-id]').each(function() {
+            var id = jQuery(this).data('dg-id');
+
+            // if we have multiple galleries, we could have multiple elements
+            // needing the same icon amd no need to request multiple times
+            if (-1 === jQuery.inArray(id, ids)) {
+                ids.push(id);
+            }
+        });
+
+        retrieveNextIcons();
+    });
+
+    /**
+     * Sends AJAX request to server requesting some of the not-yet-generated icons (if any).
+     */
+    function retrieveNextIcons() {
+        // max number of icons to retrieve per AJAX request
+        var batchLimit = 2;
+
+        // IDs already retrieved
+        var idBatch = [];
+
+        for (; i < ids.length; i++) {
+            if (idBatch.length === batchLimit) {
+                break;
+            }
+
+            idBatch.push(ids[i]);
+        }
+
+        if (idBatch.length != 0) {
+            // request the next batch of icons
+            jQuery.post(ajaxurl, { action: 'dg_generate_icons', ids: idBatch }, processRetrievedThumbnails);
+        }
+    }
+
+    /**
+     * Find all of the relevant elements and set the img src, then start next batch of thumbnail retrieval.
+     * @param response Associative array mapping attachment ID to thumbnail URL.
+     */
+    function processRetrievedThumbnails(response) {
+        for (var id in response) {
+            if (response.hasOwnProperty(id)) {
+                var target = jQuery('.document-icon[data-dg-id="' + id + '"] img');
+                (function(id, target) {
+                    target.fadeOut('fast', function () {
+                        jQuery(this).attr('src', response[id]);
+                        jQuery(this).fadeIn('fast');
+                    })
+                })(id, target);
+            }
+        }
+
+        // start next batch once this response is processed
+        retrieveNextIcons();
+    }
+})();
