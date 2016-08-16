69c38ee130a19ca8b914d2b47c7430541a778f88
Dan Rossiter <dan.rossiters@gmail.com>
2016-08-16 02:13:40 +0000

    Tagging version 4.2.4
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery@1475095 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/4.2.4/admin/tabs/thumber-co-tab.php
@@ -0,0 +1,131 @@
+<?php
+defined( 'WPINC' ) OR exit;
+
+/**
+ * Register settings for the tab.
+ */
+function dg_register_settings() {
+    global $dg_options;
+
+    add_settings_section(
+        'thumber-co', __( 'Thumber.co Subscription', 'document-gallery' ),
+        'dg_render_thumber_co_section', DG_OPTION_NAME );
+
+    add_settings_field(
+        'thumber-co_uid', __( 'User ID', 'document-gallery' ),
+        array( 'DG_Admin', 'renderTextField' ),
+        DG_OPTION_NAME, 'thumber-co',
+        array(
+            'label_for'   => 'label_thumber-co_uid',
+            'name'        => 'uid',
+            'value'       => esc_attr( $dg_options['thumber-co']['uid'] ),
+            'option_name' => DG_OPTION_NAME,
+            'description' => __( 'User ID assigned by Thumber.co', 'document-gallery' )
+        ) );
+
+    add_settings_field(
+        'thumber-co_secret', __( 'User Secret', 'document-gallery' ),
+        array( 'DG_Admin', 'renderTextField' ),
+        DG_OPTION_NAME, 'thumber-co',
+        array(
+            'label_for'   => 'label_thumber-co_secret',
+            'name'        => 'secret',
+            'value'       => esc_attr( $dg_options['thumber-co']['secret'] ),
+            'option_name' => DG_OPTION_NAME,
+            'description' => __( 'User secret assigned by Thumber.co', 'document-gallery' )
+        ) );
+}
+
+/**
+ * Validate settings for the tab.
+ */
+function dg_validate_settings( $values ) {
+    global $dg_options;
+    $ret = $dg_options;
+
+    $has_changed = $has_error = false;
+    $old_uid          = $dg_options['thumber-co']['uid'];
+    $old_secret       = $dg_options['thumber-co']['secret'];
+    $old_subscription = $dg_options['thumber-co']['subscription'];
+
+    // handle setting the UID
+    if ( isset( $values['uid'] ) && 0 !== strcmp( $values['uid'], $ret['thumber-co']['uid'] ) ) {
+        static $guid_regex = '/^[^\W_]{8}-[^\W_]{4}-[^\W_]{4}-[^\W_]{4}-[^\W_]{12}$/';
+        if ( '' ===  $values['uid'] || preg_match( $guid_regex, $values['uid'] ) ) {
+            $ret['thumber-co']['uid'] = ( '' !== $values['uid'] ) ? $values['uid'] : null;
+            $has_changed = true;
+        } else {
+            add_settings_error( DG_OPTION_NAME, 'thumber-co-uid',
+                __( 'Invalid user ID given: ', 'document-gallery' ) . $values['uid'] );
+            $has_error = true;
+        }
+    }
+
+    // handle setting the user secret
+    if ( isset( $values['secret'] ) && 0 !== strcmp( $values['secret'], $ret['thumber-co']['secret'] ) ) {
+        static $secret_regex = '/^[-_A-Z\d]+$/i';
+        if ( '' ===  $values['secret'] || preg_match( $secret_regex, $values['secret'] ) ) {
+            $ret['thumber-co']['secret'] = ( '' !==  $values['secret'] ) ? $values['secret'] : null;
+            $has_changed = true;
+        } else {
+            add_settings_error( DG_OPTION_NAME, 'thumber-co-secret',
+                __( 'Invalid user secret given: ', 'document-gallery' ) . $values['secret'] );
+            $has_error = true;
+        }
+    }
+
+    // test whether we can actually auth w/ given credentials
+    if ( $has_changed && ! $has_error ) {
+        if ( isset( $ret['thumber-co']['uid'] ) && isset( $ret['thumber-co']['secret'] ) ) {
+            include_once DG_PATH . 'inc/thumbers/thumber-co/class-thumber-co-thumber.php';
+
+            // NOTE: We're tricking getSubscription to re-query subscription w/ new credentials
+            $dg_options['thumber-co']['subscription'] = null;
+
+            $client = DG_ThumberClient::getInstance();
+            $client->setUid( $ret['thumber-co']['uid'] );
+            $client->setUserSecret( $ret['thumber-co']['secret'] );
+            $ret['thumber-co']['subscription'] = $client->getSubscription( false );
+            if ( ! isset( $ret['thumber-co']['subscription'] ) ) {
+                $ret['thumber-co']['uid']          = $old_uid;
+                $ret['thumber-co']['secret']       = $old_secret;
+                $ret['thumber-co']['subscription'] = $old_subscription;
+
+                add_settings_error( DG_OPTION_NAME, 'thumber-co-authenticate',
+                    __( 'Failed to authenticate with given user ID and secret.', 'document-gallery' ) );
+            } else {
+                // auto-enable if we've got newly-working credentials
+                $ret['thumber']['active']['thumber-co'] = true;
+            }
+        } else {
+            // auto-disable if we've got newly-broken credentials
+            $ret['thumber']['active']['thumber-co'] = false;
+        }
+    }
+
+    return $ret;
+}
+
+/**
+ * Render the Thumber Advanced section.
+ */
+function dg_render_thumber_co_section() { ?>
+    <p><?php _e( '<em>Thumber.co</em> is an external service capable of generating ' .
+                 'thumbnails for the tricky file types that Document Gallery can\'t handle on its own. Files such as ' .
+                 'Word, PowerPoint, and PDFs can all be processed, allowing you to provide a more complete experience ' .
+                 'to your users. Further information, including a complete list of supported file types is available '.
+                 '<a href="https://www.thumber.co/about" target="_blank">here</a>.', 'document-gallery' ); ?></p>
+
+    <p><?php _e( 'Once you <a href="https://www.thumber.co/subscriptions">register for a Thumber.co subscription</a>, simply enter your credentials below to get started!' ); ?></p>
+    <?php if ( ! DG_Util::isPublicSite() ) : ?>
+        <p>
+            <em><?php _e( 'NOTE: It appears that you are on a private server not accessible from outside your local network. ' .
+                    'Thumber must be able to access your site in order for thumbnail conversions to work properly.', 'document-gallery' ); ?></em>
+        </p>
+    <?php endif; ?>
+    <?php if ( !in_array( 'sha256', hash_algos() ) ) : ?>
+        <p>
+            <em><?php _e( 'NOTE: Your server does not support SHA-256 hashing. You will not be able to communicate with Thumber.', 'document-gallery' ); ?></em>
+        </p>
+    <?php endif; ?>
+<?php }
---
+++ tags/4.2.4/inc/thumbers/class-ghostscript-thumber.php
@@ -0,0 +1,158 @@
+<?php
+defined( 'WPINC' ) OR exit;
+
+class DG_GhostscriptThumber extends DG_AbstractThumber {
+
+	/**
+	 * Initialize the thumber class for use in thumbnail generation.
+	 */
+	public static function init() {
+		$options  = DG_Thumber::getOptions();
+		$active   = $options['active'];
+		if ( $active['gs'] && self::isGhostscriptAvailable() ) {
+			parent::init();
+		}
+	}
+
+	/**
+	 * Get thumbnail for document with given ID using Ghostscript. Imagick could
+	 * also handle this, but is *much* slower.
+	 *
+	 * @param int $ID The attachment ID to retrieve thumbnail from.
+	 * @param int $pg The page number to make thumbnail of -- index starts at 1.
+	 *
+	 * @return bool|string  False on failure, URL to thumb on success.
+	 */
+	public function getThumbnail($ID, $pg = 1) {
+		static $gs = null;
+
+		if ( is_null( $gs ) ) {
+			$options = DG_Thumber::getOptions();
+			$gs      = $options['gs'];
+
+			if ( false !== $gs ) {
+				$gs = escapeshellarg( $gs ) . ' -sDEVICE=png16m -dFirstPage=%1$d'
+					. ' -dLastPage=%1$d -dBATCH -dNOPAUSE -dPDFFitPage -sOutputFile=%2$s %3$s 2>&1';
+			}
+		}
+
+		if ( false === $gs ) {
+			return false;
+		}
+
+		$doc_path  = get_attached_file( $ID );
+		$temp_path = DG_Util::getTempFile();
+
+		$old_locale = setlocale( LC_CTYPE, 0 );
+		setlocale( LC_CTYPE, 'en_US.UTF-8' );
+		$cmd = sprintf( $gs, $pg, escapeshellarg( $temp_path ), escapeshellarg( $doc_path ) );
+		if ( false !== $old_locale ) {
+			setlocale( LC_CTYPE, $old_locale );
+		}
+
+		DG_Logger::writeLog( DG_LogLevel::Detail, 'Executing: ' . $cmd );
+		exec( $cmd, $out, $ret );
+
+		if ( $ret != 0 ) {
+			DG_Logger::writeLog( DG_LogLevel::Error, __( 'Ghostscript failed: ', 'document-gallery' ) . print_r( $out ) );
+			@unlink( $temp_path );
+
+			return false;
+		}
+
+		return $temp_path;
+	}
+	/**
+	 * @return string[] The extensions supported by this thumber.
+	 */
+	protected function getThumberExtensions() {
+		return array( 'pdf', 'ps', 'eps' );
+	}
+
+	/**
+	 * Dynamically determines whether we may call gs through exec().
+	 *
+	 * NOTE: This does not check the options for gs path. Don't use in
+	 * thumbnail generation as it's slow and not configurable.
+	 *
+	 * @return bool|string If available, returns exe path. False otherwise.
+	 */
+	public static function getGhostscriptExecutable() {
+		static $executable = null;
+
+		if ( is_null( $executable ) ) {
+			// we must be able to exec()
+			$executable = self::isExecAvailable();
+			if ( ! $executable ) {
+				return $executable;
+			}
+
+			// find on Windows system
+			if ( 'WIN' === strtoupper( substr( PHP_OS, 0, 3 ) ) ) {
+				// look for environment variable
+				$executable = getenv( 'GSC' );
+				if ( $executable ) {
+					return $executable;
+				}
+
+				// hope GS in the path
+				$executable = exec( 'where gswin*c.exe' );
+				if ( ! empty( $executable ) ) {
+					return $executable;
+				}
+
+				// look directly in filesystem
+				// 64- or 32-bit binary
+				$executable = exec( 'dir /o:n/s/b "C:\Program Files\gs\*gswin*c.exe"' );
+				if ( ! empty( $executable ) ) {
+					return $executable;
+				}
+
+				// 32-bit binary on 64-bit OS
+				$executable = exec( 'dir /o:n/s/b "C:\Program Files (x86)\gs\*gswin32c.exe"' );
+				$executable = empty( $executable ) ? false : $executable;
+
+				return $executable;
+			}
+
+			// handle Linux systems
+			$executable = exec( 'which gs' );
+			if ( ! empty( $executable ) ) {
+				return $executable;
+			}
+
+			// GoDaddy and others aren't setup in such a way that
+			// the above works so we need to fallback to a direct
+			// filesystem check in most common location
+			exec( 'test -e /usr/bin/gs', $dummy, $ret );
+			$executable = ( $ret === 0 ) ? '/usr/bin/gs' : false;
+
+			return $executable;
+		}
+
+		return $executable;
+	}
+
+	/**
+	 * @return bool Whether we can use the GS executable.
+	 */
+	public static function isGhostscriptAvailable() {
+		static $ret = null;
+
+		if ( is_null( $ret ) ) {
+			$options = DG_Thumber::getOptions();
+			$ret     = $options['gs'] && self::isExecAvailable();
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * @return int An integer from 0 to 100. Higher priorities will be attempted before lower priority thumbers.
+	 */
+	public function getPriority() {
+		return 75;
+	}
+}
+
+DG_GhostscriptThumber::init();
