416774125559b41b871304a1728cf83dfe95bcfc
Dan Rossiter <dan.rossiters@gmail.com>
2017-05-02 03:33:14 +0000

    Resolves bug in caching of $exts.
    git-svn-id: http://plugins.svn.wordpress.org/document-gallery/trunk@1649315 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/thumbers/class-abstract-thumber.php
+++ inc/thumbers/class-abstract-thumber.php
@@ -145,7 +145,7 @@ abstract class DG_AbstractThumber {
 	 * @return string[] Contains all MIME types supported by WordPress, including custom types added by plugins.
 	 */
 	protected static function getAllExts() {
-		$exts = null;
+		static $exts = null;
 
 		if ( is_null( $exts ) ) {
 			$exts = array_keys( array_merge( wp_get_mime_types(), get_allowed_mime_types() ) );
